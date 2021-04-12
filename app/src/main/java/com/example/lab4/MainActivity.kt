4bcd5660c1d7efc2c3e1458aa7999da28cb59eff
thekey99 <ahmedyounis@gmail.com>
2021-04-12 20:27:17 +0300

    done

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,3 +1,8 @@
+
+
+
+// Students: Abdulaziz Al-Kandari & Ahmad Younis. Collaberated with: Feras El Sabaa & Ali Kasem
+
 package com.example.lab4
 
 import androidx.appcompat.app.AppCompatActivity
@@ -5,18 +10,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 
-//import kotlinx.android.synthetic.main.activity_main.*
+// import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     //        lateinit var nprints: EditText
-    var x= 19
-    var y= 49
-    var z= 79
+    var x= 0.19
+    var y= 0.49
+    var z= 0.79
 
     var radioGroup: RadioGroup? = null
     lateinit var radioButton: RadioButton
     private lateinit var button: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         // Assigning id of RadioGroup
         radioGroup = findViewById(R.id.radioGroup)
         var nprints = findViewById<EditText>(R.id.nprints)
+      // var rb1= findViewById<>()
 
         // Assigning id of Submit button
         button = findViewById(R.id.button)
@@ -41,24 +48,32 @@ class MainActivity : AppCompatActivity() {
             val selectedOption: Int = radioGroup!!.checkedRadioButtonId
 //                var ans  = null
 // Assigning id of the checked radio button
-            radioButton = findViewById(selectedOption)
+          //  radioButton = findViewById(selectedOption)
+
+
 //
-            if (selectedOption.toString().equals("2131231013")){
+          if (selectedOption.toString().equals("2131231013"))
+          {
                 var ans = x * (nprints.text.toString().toInt())
-                textView.setText("The order cost is $ans")
+              textView.setText("The order cost is $ $ans")
 
             } else if (selectedOption.toString().equals("2131231014")){
                 var ans = y * nprints.text.toString().toInt()
-                textView.setText("The order cost is $ans")
-//                                var ans =  nprints.text.toString().toInt()
-            }else {
+                textView.setText("The order cost is $ $ans")
+//                             var ans =  nprints.text.toString().toInt()
+            } else if (selectedOption.toString().equals("2131231015")){
                 var ans = z * nprints.text.toString().toInt()
-                textView.setText("The order cost is $ans")
-            }
+                textView.setText("The order cost is $ $ans")
+         }
 //                val answer
 
+
+
+
+
+
             // Displaying text of the checked radio button in the form of toast
-//                Toast.makeText(this, selectedOption.toString()  , Toast.LENGTH_SHORT).show()
+ // Toast.makeText(this, selectedOption.toString()  , Toast.LENGTH_SHORT).show()
 
                 }
     }
